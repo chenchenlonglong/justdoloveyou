d4382450ba3b8ba9906a76f2910eb1e020e55b20
chenlong <1004076836@qq.com>
2016-09-09 10:25:36 +0800

    测试

--- config/web.php
+++ config/web.php
@@ -2,13 +2,18 @@
 
 $params = require(__DIR__ . '/params.php');
 
+Yii::$classMap['Functions'] = '@app/libs/Functions.php';
+
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'index', //默认控制器
+    'modules'=>[
+        'admin'=>['class' => 'app\modules\admin\Admin']
+    ],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'justdoloveyou_',
         ],
         'cache' => [
---
+++ libs/Functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * author: chenlong
+ * Date: 2016/9/9
+ * Time: 10:14
+ */
+class Functions
+{
+
+}
