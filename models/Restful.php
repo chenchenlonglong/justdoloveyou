8ba6ab9dbc14130a91b7e5daa6613d65ae2d2032
chenchenlonglong <1004076836@qq.com>
2016-11-23 11:06:27 +0800

    11

--- models/Restful.php
+++ models/Restful.php
@@ -15,4 +15,13 @@ class Restful extends  ActiveRecord
         public static  function  tableName(){
             return "{{restful}}";
         }
+
+
+// 明确列出每个字段，适用于你希望数据表或模型属性修改时不导致你的字段修改（保持后端API兼容性）
+        public    function fields(){
+            return [
+                "id",
+                "name"=>"c_name",
+            ];
+        }
 }
