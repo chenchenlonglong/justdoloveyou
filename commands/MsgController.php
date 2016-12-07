49c091f7d024e75413d2308c518cdf142f5e6fee
chenchenlonglong <1004076836@qq.com>
2016-12-07 16:33:28 +0800

    11

--- commands/MsgController.php
+++ commands/MsgController.php
@@ -19,13 +19,21 @@ class MsgController extends  Controller
 
     public  function  actionGet_result(){
         $curl= new Curl();
-        $num=0;
-        for($i=0;$i<=2000;$i++){
+        $num_1=$num_2=$num_3=0;
+        for($i=0;$i<100;$i++){
             $result=$curl->get("http://test.order.sochepiao.com/index.php?r=trip/send_bouns&msign=leyou@hcp");
+            $msg[]=$result;
             if($result==1){
-                $num++;
+                ++$num_1;
+            }
+            if($result==2){
+                ++$num_2;
+            }
+            if($result==3){
+                ++$num_3;
             }
         }
-        echo $num;
+        echo "最大红包的次数:". $num_1."<br/>";
+        echo "普通红包的次数:". $num_2."<br/>";
+        echo "优惠券的次数:". $num_3."<br/>";
     }
-}
