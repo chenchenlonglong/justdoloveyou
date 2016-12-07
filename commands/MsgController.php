5dd3e16b895977166a02a304fd5bab01455a374d
chenchenlonglong <1004076836@qq.com>
2016-12-07 16:36:12 +0800

    11

--- commands/MsgController.php
+++ commands/MsgController.php
@@ -13,27 +13,30 @@ use yii\curl\Curl;
 
 class MsgController extends  Controller
 {
-    public function  actionIndex(){
+    public function  actionIndex()
+    {
         $curl = new Curl();
     }
 
-    public  function  actionGet_result(){
-        $curl= new Curl();
-        $num_1=$num_2=$num_3=0;
-        for($i=0;$i<100;$i++){
-            $result=$curl->get("http://test.order.sochepiao.com/index.php?r=trip/send_bouns&msign=leyou@hcp");
-            $msg[]=$result;
-            if($result==1){
+    public function  actionGet_result()
+    {
+        $curl = new Curl();
+        $num_1 = $num_2 = $num_3 = 0;
+        for ($i = 0; $i < 100; $i++) {
+            $result = $curl->get("http://test.order.sochepiao.com/index.php?r=trip/send_bouns&msign=leyou@hcp");
+            $msg[] = $result;
+            if ($result == 1) {
                 ++$num_1;
             }
-            if($result==2){
+            if ($result == 2) {
                 ++$num_2;
             }
-            if($result==3){
+            if ($result == 3) {
                 ++$num_3;
             }
         }
-        echo "最大红包的次数:". $num_1."<br/>";
-        echo "普通红包的次数:". $num_2."<br/>";
-        echo "优惠券的次数:". $num_3."<br/>";
+        echo "最大红包的次数:" . $num_1 . "<br/>";
+        echo "普通红包的次数:" . $num_2 . "<br/>";
+        echo "优惠券的次数:" . $num_3 . "<br/>";
     }
+}
